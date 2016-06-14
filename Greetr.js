634d8c511eb4488d6535300add0a2371609c0f6e
Pedro Teixeira <pedro.ap.teixeira@gmail.com>
2016-06-14 18:38:51 +0100

    Added more functionality

--- Greetr.js
+++ Greetr.js
@@ -1,14 +1,80 @@
 (function(global, $) {
 
-  var Greetr = function(firstName, lastName, language) {
+  const Greetr = function(firstName, lastName, language) {
     return new Greetr.init(firstName, lastName, language);
   }
 
-  Greetr.prototype = {};
+  const supportedLanguages = ["en", "es"];
+
+  const greetings = {
+    en: "Hello",
+    es: "Hola"
+  };
+
+  const formalGreetings = {
+    en: "Greetings",
+    es: "Saludos"
+  };
+
+  var logMessages = {
+    en: "Logged in",
+    es: "Inició sesión"
+  };
+
+  Greetr.prototype = {
+    fullName: function() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+
+    validate: function() {
+      if (supportedLanguages.indexOf(this.language) === -1) {
+        throw "Invalid Language";
+      }
+    },
+
+    greeting: function() {
+      return `${greetings[this.language]} ${this.firstName}!`;
+    },
+
+    formalGreeting: function() {
+      return `${formalGreetings[this.language]}, ${this.fullName()}`;
+    },
+
+    greet: function(formal) {
+      var msg;
+
+      if (formal) {
+        msg = this.formalGreeting();
+      } else {
+        msg = this.greeting();
+      }
+
+      if (console) {
+        console.log(msg);
+      }
+
+      // for method chaining
+      return this;
+    },
+
+    log: function() {
+      if (console) {
+        console.log(`${logMessages[this.language]}: ${this.fullName()}`);
+      }
+
+      return this;
+    },
+
+    setLang: function(lang) {
+      this.language = lang;
+      this.validate();
+      return this;
+    }
+  };
 
   Greetr.init = function(firstName, lastName, language) {
 
-    var self = this;
+    const self = this;
     self.firstName = firstName || "";
     self.lastName = lastName || "";
     self.language = language || "en";
--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 var g = G$("john", "doe");
-console.log(g)
+g.greet().setLang("es").greet(true);
