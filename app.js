b401025c1845796edfe69f4dd661f308eb9a1958
Pedro Teixeira <pedro.ap.teixeira@gmail.com>
2016-06-14 19:00:47 +0100

    Added more stuff

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 var g = G$("john", "doe");
+
 g.greet().setLang("es").greet(true).setLang("en").log();
+
+$("#login").click(() => {
+  var loginGrtr = G$("John", "Doe");
+
+  $("#logindiv").hide();
+
+  loginGrtr.setLang($("#lang").val()).HTMLGreeting("#greeting", true).log();
+});
